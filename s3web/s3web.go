19587051ac7b1b0978719768838f723d431043df
Eric Schrock <Eric.Schrock@delphix.com>
2020-01-03 14:17:07 -0500

    implementation

--- s3web/s3web.go
+++ s3web/s3web.go
@@ -9,6 +9,7 @@ import (
 	"github.com/titan-data/remote-sdk-go/pkg/remote"
 	"net/url"
 	"reflect"
+	"strings"
 )
 
 type s3webRemote struct {
@@ -19,20 +20,29 @@ func (s s3webRemote) Type() string {
 }
 
 func (s s3webRemote) FromURL(url url.URL, additionalProperties map[string]string) (map[string]interface{}, error) {
-	// nop remotes can only be "nop", which means everything other than "path" must be empty
-	if url.Scheme != "" || url.Host != "" || url.User != nil || url.Path != "nop" {
-		return nil, errors.New("malformed remote identifier")
+	if url.Scheme != "s3web" {
+		return nil, errors.New("invalid remote scheme")
+	}
+
+	if url.User != nil {
+		return nil, errors.New("username and password cannot be specified")
+	}
+
+	if url.Hostname() == "" {
+		return nil, errors.New("missing host name in remote")
 	}
 
 	if len(additionalProperties) != 0 {
 		return nil, errors.New(fmt.Sprintf("invalid property '%s'", reflect.ValueOf(additionalProperties).MapKeys()[0].String()))
 	}
 
-	return map[string]interface{}{}, nil
+	u := fmt.Sprintf("http://%s%s", url.Host, url.Path)
+	return map[string]interface{}{"url": u}, nil
 }
 
 func (s s3webRemote) ToURL(properties map[string]interface{}) (string, map[string]string, error) {
-	return "s3web", map[string]string{}, nil
+	u := properties["url"].(string)
+	return strings.Replace(u, "http", "s3web", 1), map[string]string{}, nil
 }
 
 func (s s3webRemote) GetParameters(remoteProperties map[string]interface{}) (map[string]interface{}, error) {
