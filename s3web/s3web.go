0fa4de5782c6544a019fe207eaad49080bba34a7
Eric Schrock <Eric.Schrock@delphix.com>
2020-01-19 16:02:56 -0500

    remaining tests

--- s3web/s3web.go
+++ s3web/s3web.go
@@ -4,9 +4,13 @@
 package s3web
 
 import (
+	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/titan-data/remote-sdk-go/remote"
+	"io/ioutil"
+	"net/http"
 	"net/url"
 	"reflect"
 	"strings"
@@ -55,19 +59,68 @@ func (s s3webRemote) GetParameters(remoteProperties map[string]interface{}) (map
 }
 
 func (s s3webRemote) ValidateRemote(properties map[string]interface{}) error {
-	panic("implement me")
+	return remote.ValidateFields(properties, []string{"url"}, []string{})
 }
 
 func (s s3webRemote) ValidateParameters(parameters map[string]interface{}) error {
-	panic("implement me")
+	return remote.ValidateFields(parameters, []string{}, []string{})
+}
+
+var httpGet = http.Get
+
+type MetadataCommit struct {
+	Id         string                 `json:"id"`
+	Properties map[string]interface{} `json:"properties"`
 }
 
 func (s s3webRemote) ListCommits(properties map[string]interface{}, parameters map[string]interface{}, tags []remote.Tag) ([]remote.Commit, error) {
-	panic("implement me")
+	metadataPath := fmt.Sprintf("%s/%s", properties["url"], "titan")
+	resp, err := httpGet(metadataPath)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode == http.StatusNotFound {
+		return []remote.Commit{}, nil
+	}
+
+	if resp.StatusCode >= 300 {
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("failed to get '%s': %s", metadataPath, string(b))
+	}
+
+	var ret []remote.Commit
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if (line) != "" {
+			commit := MetadataCommit{}
+			err = json.Unmarshal([]byte(line), &commit)
+			if err == nil && commit.Properties != nil && commit.Id != "" && remote.MatchTags(commit.Properties, tags) {
+				ret = append(ret, remote.Commit{Id: commit.Id, Properties: commit.Properties})
+			}
+		}
+	}
+
+	remote.SortCommits(ret)
+
+	return ret, nil
 }
 
 func (s s3webRemote) GetCommit(properties map[string]interface{}, parameters map[string]interface{}, commitId string) (*remote.Commit, error) {
-	panic("implement me")
+	commits, err := s.ListCommits(properties, parameters, []remote.Tag{})
+	if err != nil {
+		return nil, err
+	}
+	for _, c := range commits {
+		if c.Id == commitId {
+			return &c, nil
+		}
+	}
+
+	return nil, nil
 }
 
 func init() {
