8bc15d594e4d82d6d6d429f89c547c6b5dd8cc25
Eric Schrock <Eric.Schrock@delphix.com>
2020-01-18 21:15:29 -0500

    update remote-sdk-go

--- s3web/s3web.go
+++ s3web/s3web.go
@@ -20,20 +20,20 @@ func (s s3webRemote) Type() (string, error) {
 }
 
 func (s s3webRemote) FromURL(rawUrl string, additionalProperties map[string]string) (map[string]interface{}, error) {
-	url, err := url.Parse(rawUrl)
+	u, err := url.Parse(rawUrl)
 	if err != nil {
 		return nil, err
 	}
 
-	if url.Scheme != "s3web" {
+	if u.Scheme != "s3web" {
 		return nil, errors.New("invalid remote scheme")
 	}
 
-	if url.User != nil {
+	if u.User != nil {
 		return nil, errors.New("remote username and password cannot be specified")
 	}
 
-	if url.Hostname() == "" {
+	if u.Hostname() == "" {
 		return nil, errors.New("missing remote host name")
 	}
 
@@ -41,8 +41,8 @@ func (s s3webRemote) FromURL(rawUrl string, additionalProperties map[string]stri
 		return nil, errors.New(fmt.Sprintf("invalid property '%s'", reflect.ValueOf(additionalProperties).MapKeys()[0].String()))
 	}
 
-	u := fmt.Sprintf("http://%s%s", url.Host, url.Path)
-	return map[string]interface{}{"url": u}, nil
+	res := fmt.Sprintf("http://%s%s", u.Host, u.Path)
+	return map[string]interface{}{"url": res}, nil
 }
 
 func (s s3webRemote) ToURL(properties map[string]interface{}) (string, map[string]string, error) {
@@ -52,6 +52,22 @@ func (s s3webRemote) ToURL(properties map[string]interface{}) (string, map[strin
 
 func (s s3webRemote) GetParameters(remoteProperties map[string]interface{}) (map[string]interface{}, error) {
 	return map[string]interface{}{}, nil
+}
+
+func (s s3webRemote) ValidateRemote(properties map[string]interface{}) error {
+	panic("implement me")
+}
+
+func (s s3webRemote) ValidateParameters(parameters map[string]interface{}) error {
+	panic("implement me")
+}
+
+func (s s3webRemote) ListCommits(properties map[string]interface{}, parameters map[string]interface{}, tags []remote.Tag) ([]remote.Commit, error) {
+	panic("implement me")
+}
+
+func (s s3webRemote) GetCommit(properties map[string]interface{}, parameters map[string]interface{}, commitId string) (*remote.Commit, error) {
+	panic("implement me")
 }
 
 func init() {
--- s3web/s3web_test.go
+++ s3web/s3web_test.go
@@ -11,73 +11,85 @@ import (
 
 func TestRegistered(t *testing.T) {
 	r := remote.Get("s3web")
-	ret, _ := r.Type()
-	assert.Equal(t, "s3web", ret)
+	ret, err := r.Type()
+	if assert.NoError(t, err) {
+		assert.Equal(t, "s3web", ret)
+	}
 }
 
 func TestFromURL(t *testing.T) {
 	r := remote.Get("s3web")
-	props, _ := r.FromURL("s3web://host/object/path", map[string]string{})
-	assert.Equal(t, "http://host/object/path", props["url"])
+	props, err := r.FromURL("s3web://host/object/path", map[string]string{})
+	if assert.NoError(t, err) {
+		assert.Equal(t, "http://host/object/path", props["url"])
+	}
 }
 
 func TestNoPath(t *testing.T) {
 	r := remote.Get("s3web")
-	props, _ := r.FromURL("s3web://host", map[string]string{})
-	assert.Equal(t, "http://host", props["url"])
+	props, err := r.FromURL("s3web://host", map[string]string{})
+	if assert.NoError(t, err) {
+		assert.Equal(t, "http://host", props["url"])
+	}
 }
 
 func TestBadProperty(t *testing.T) {
 	r := remote.Get("s3web")
 	_, err := r.FromURL("s3web://host", map[string]string{"a": "b"})
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestBadScheme(t *testing.T) {
 	r := remote.Get("s3web")
 	_, err := r.FromURL("s3web", map[string]string{})
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestBadSchemeName(t *testing.T) {
 	r := remote.Get("s3web")
 	_, err := r.FromURL("foo://bar", map[string]string{})
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestBadUser(t *testing.T) {
 	r := remote.Get("s3web")
 	_, err := r.FromURL("s3web://user@host/path", map[string]string{})
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestBadUserPassword(t *testing.T) {
 	r := remote.Get("s3web")
 	_, err := r.FromURL("s3web://user:password@host/path", map[string]string{})
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestBadNoHost(t *testing.T) {
 	r := remote.Get("s3web")
 	_, err := r.FromURL("s3web:///path", map[string]string{})
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestPort(t *testing.T) {
 	r := remote.Get("s3web")
-	props, _ := r.FromURL("s3web://host:1023/object/path", map[string]string{})
-	assert.Equal(t, "http://host:1023/object/path", props["url"])
+	props, err := r.FromURL("s3web://host:1023/object/path", map[string]string{})
+	if assert.NoError(t, err) {
+		assert.Equal(t, "http://host:1023/object/path", props["url"])
+	}
 }
 
 func TestToURL(t *testing.T) {
 	r := remote.Get("s3web")
-	u, props, _ := r.ToURL(map[string]interface{}{"url": "http://host/path"})
-	assert.Equal(t, "s3web://host/path", u)
-	assert.Empty(t, props)
+	u, props, err := r.ToURL(map[string]interface{}{"url": "http://host/path"})
+	if assert.NoError(t, err) {
+		assert.Equal(t, "s3web://host/path", u)
+		assert.Empty(t, props)
+	}
 }
 
 func TestParameters(t *testing.T) {
 	r := remote.Get("s3web")
-	props, _ := r.GetParameters(map[string]interface{}{"url": "http://host/path"})
-	assert.Empty(t, props)
+	props, err := r.GetParameters(map[string]interface{}{"url": "http://host/path"})
+	if assert.NoError(t, err) {
+		assert.Empty(t, props)
+	}
 }
